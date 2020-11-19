7ae65ba42efb54e25f08b5bac73fee284a6dd81e
JonatanRDiaz <70856243+JonatanRDiaz@users.noreply.github.com>
2020-11-18 21:36:26 -0300

    Add files via upload

---
+++ JAVASCRIPTS/ENTREGA2/js/main.js
@@ -0,0 +1,14 @@
+//PROGRAMA EN ARCHIVO JAVASCRIPT 
+
+
+
+var montodecompra = parseInt(prompt("Ingrese el monto de su compra"));
+
+if( montodecompra > 1000 ){
+    alert("Su compra es mayorista");
+
+}else if (montodecompra < 1000) {
+    alert("Su compra es minorista");
+}
+
+
