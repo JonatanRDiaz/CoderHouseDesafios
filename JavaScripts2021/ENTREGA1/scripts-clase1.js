a1336c2f874f3dfaba5b50054e7be2b94f745eda
JonatanRDiaz <70856243+JonatanRDiaz@users.noreply.github.com>
2021-02-24 00:55:36 -0300

    Add files via upload

---
+++ JavaScripts2021/ENTREGA1/scripts-clase1.js
@@ -0,0 +1,30 @@
+
+
+
+
+      // ALUMNO: DIAZ JONATAN - CAMADA 14170
+
+
+
+        // PRIMER EJEMPLO 
+        // 1 - capturar entadas - 
+        var varNombre = prompt("INGRESE SU NOMBRE");
+        var varApellido = prompt("INGRESE SU APELLIDO");
+        // 2 - PROCESAR ENTRADAS
+        var resultado =varNombre+" "+varApellido;
+        // 3 - MOSTRAR SALIDA
+        console.log(resultado);
+        alert("Bienvenido " + resultado);
+  
+  
+
+
+    // SEGUNDO EJEMPLO    
+   // 1 - capturar entadas - 
+   var varPrecio1 = prompt("INGRESE EL PRIMER PRECIO");
+        var varPrecio2 = prompt("INGRESE EL SEGUNDO PRECIO");
+        // 2 - PROCESAR ENTRADAS
+        var resultado =parseInt (varPrecio1)+ parseInt (varPrecio2);
+        // 3 - MOSTRAR SALIDA
+        console.log(resultado);
+        alert("El monto total es" + resultado);
