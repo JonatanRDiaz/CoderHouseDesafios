847e69e3627acd25bd2e67918a142341354d25c0
JonatanRDiaz <70856243+JonatanRDiaz@users.noreply.github.com>
2021-03-01 23:58:46 -0300

    Add files via upload

---
+++ JavaScripts2021/ENTREGA2/js/main.js
@@ -0,0 +1,17 @@
+
+
+ // ALUMNO: DIAZ JONATAN - CAMADA 14170
+
+ //PROGRAMA EN ARCHIVO JAVASCRIPT 
+
+
+var montodecompra = parseInt(prompt("Ingrese el monto de su compra"));
+
+if( montodecompra > 1000 ){
+    alert("Su compra es mayorista");
+
+}else if (montodecompra < 1000) {
+    alert("Su compra es minorista");
+}
+
+
